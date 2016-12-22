3897a56552556434cea6d19e3fa5b96179cab88a
Maria E. Chacon <maria.chacon@globant.com>
2016-12-22 14:46:22 -0300

    Added method difference and intersection for  2 sets

--- practica1/sets.py
+++ practica1/sets.py
@@ -7,3 +7,12 @@ class Set:
 
     def remove_element(self, element):
         self.list.remove(element)
+
+    @classmethod
+    def diff(clr, a, b): #difference
+        print([x for x in a.list if x not in b.list] + [x for x in b.list if x not in a.list])
+    
+    @classmethod
+    def inter(clr, a, b):  #intersection
+        print(list(set([x for x in a.list if x in b.list] + [x for x in b.list if x in a.list])))
+
