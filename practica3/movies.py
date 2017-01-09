df65fdd48a89c7c1c3619798dfa1bf72799d17e1
Maria E. Chacon <maria.chacon@globant.com>
2017-01-09 13:52:05 -0300

    added html file

--- practica3/movies.py
+++ practica3/movies.py
@@ -5,6 +5,7 @@ csv_f = csv.reader(f)
 #color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,movie_title,num_voted_users,cast_total_facebook_likes,actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes
 movie_color = 0
 movie_bw    = 0
+directors   = []
 for row in csv_f:
     #print(row)
     color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,movie_title,num_voted_users,cast_total_facebook_likes,actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes = row
@@ -12,6 +13,27 @@ for row in csv_f:
         movie_color = movie_color + 1
     if color == ' Black and White':
         movie_bw    = movie_bw + 1
+    if director_name not in directors:
+            directors.append(director_name)
 
-print("{} movies Color".format(movie_color))
-print("{} movies Black and White".format(movie_bw))
+html_str = """
+<table border=1>
+     <tr>
+       <th>Pregunta</th>
+       <th>Respuesta</th>
+     </tr>
+     <indent>
+       <tr>
+         <td>Cuantas peliculas a 'color' y 'blanco y negro' hay en la lista?</td>
+         <td> {} Peliculas a color y {} en Blanco y Negro</td>
+       </tr>
+     </indent>
+</table>
+""".format(movie_color, movie_bw)
+
+Html_file= open("movies-report.html","w")
+Html_file.write(html_str)
+Html_file.close()
+
+# print("{} movies Color".format(movie_color))
+# print("{} movies Black and White".format(movie_bw))
